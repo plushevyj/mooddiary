6ebda7e8236aac6a154e3a3b4530334968dd2522
kirill <ischencko.kir@gmail.com>
2022-10-19 14:39:31 +0300

    Добавляет сливер

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-import '/layout/style.dart' as custom_colors;
+import '/layout/custom_colors.dart' as custom_colors;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -19,42 +20,17 @@ class _HomeScreenState extends State<HomeScreen> {
     super.initState();
   }
 
-  double get _horizontalTitlePadding {
-    const kBasePadding = 15.0;
-    const kMultiplier = 0.5;
-    const kExpandedHeight = 200;
-
-    if (_scrollController.hasClients) {
-      if (_scrollController.offset < (kExpandedHeight / 2)) {
-        // In case 50%-100% of the expanded height is viewed
-        return kBasePadding;
-      }
-
-      if (_scrollController.offset > (kExpandedHeight - kToolbarHeight)) {
-        // In case 0% of the expanded height is viewed
-        return (kExpandedHeight / 2 - kToolbarHeight) * kMultiplier +
-            kBasePadding;
-      }
-
-      // In case 0%-50% of the expanded height is viewed
-      return (_scrollController.offset - (kExpandedHeight / 2)) * kMultiplier +
-          kBasePadding;
-    }
-
-    return kBasePadding;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFFFFFFF),
       body: CustomScrollView(
+        controller: _scrollController,
         slivers: [
           SliverAppBar(
             pinned: true,
-            floating: true,
             collapsedHeight: 70,
-            expandedHeight: 200,
+            expandedHeight: 400,
             backgroundColor: custom_colors.appBarColor,
             leading: const Padding(
               padding: EdgeInsets.only(left: 15),
@@ -62,15 +38,28 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             leadingWidth: 50,
             flexibleSpace: FlexibleSpaceBar(
-              titlePadding: EdgeInsets.symmetric(
-                  vertical: 16.0, horizontal: _horizontalTitlePadding),
-              // centerTitle: true,
+              titlePadding:
+                  EdgeInsets.only(bottom: 25, left: _horizontalTitlePadding, right: _horizontalTitlePadding),
               title: Text(
                 'October 7, 2022',
                 style: GoogleFonts.readexPro(color: const Color(0xFFFFFFFF)),
               ),
+              background: Center(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 24, vertical: 50),
+                  child: TableCalendar(
+                    currentDay: DateTime.now(),
+                    focusedDay: DateTime.now(),
+                    firstDay: DateTime(2020, 1, 1),
+                    lastDay: DateTime(2025, 12, 31),
+                    headerVisible: false,
+                    rowHeight: 40,
+
+                  ),
+                ),
+              ),
             ),
-            centerTitle: true,
             actions: const [
               Icon(Icons.search, color: Color(0xFFFFFFFF)),
               SizedBox(width: 15),
@@ -84,19 +73,38 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
           SliverFillRemaining(
+            hasScrollBody: false,
             child: Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text('Increment value is: '),
-                  SizedBox(height: 10),
-                  Text('10'),
-                ],
+                children: [for (int i = 0; i < 50; i++) const Text('kek')],
               ),
             ),
           )
         ],
       ),
     );
+  }
+
+  double get _horizontalTitlePadding {
+    const kBasePadding = 15.0;
+    const kMultiplier = 0.8;
+    const kExpandedHeight = 370;
+
+    if (_scrollController.hasClients) {
+      // In case 50%-100% of the expanded height is viewed
+      if (_scrollController.offset < (kExpandedHeight / 2)) {
+        return kBasePadding;
+      }
+      // In case 0% of the expanded height is viewed
+      if (_scrollController.offset > (kExpandedHeight - kToolbarHeight)) {
+        return (kExpandedHeight / 2 - kToolbarHeight) * kMultiplier +
+            kBasePadding;
+      }
+      // In case 0%-50% of the expanded height is viewed
+      return (_scrollController.offset - (kExpandedHeight / 2)) * kMultiplier +
+          kBasePadding;
+    }
+    return kBasePadding;
   }
 }
